b92e06ae0a9a3cc097a36e23c5b869e798ea969a
Brandon Halls <brandonc.halls@gmail.com>
2014-05-28 15:28:02 -0400

    Update TurnManager.java

--- Runebind/src/com/ravine/runebind/turn/TurnManager.java
+++ Runebind/src/com/ravine/runebind/turn/TurnManager.java
@@ -54,16 +54,17 @@ public class TurnManager {
 
 
     /*
-        Refresh - player.refreshCards
+        Refresh - player.refreshCards -- One time step
         Movement -  board.highlightAdjacent (player may move one tile) if not then
          player.setRest(0-4) (player may set aside 0-4 dice to discard fat) then
-          diceManager.rollDice board.highlightPaths
+          diceManager.rollDice board.highlightPaths -- Does not need a loop. Use flags for each part of step
         Adventure - player.adventure? yes: adventureDeck.draw(color)
          event: board.replaceEvent board.applyEvent board.replenish?
           encounter: player.resolve(card)
           challenge: player.resolve(card)
           unresolved challenge will be handled as an adventure
           pvp later
+        Story - add just to allow for the addition of expansions
         Market - marketDeck*Town.push(marketDeckAll.pop) board.showMarketOptions
          buy item/ally: show current towns market cards. card.clicked? player.confirmBuy? playerHand.add(Card)
          sell card: player.showHand card.clicked? player.confirmSell
